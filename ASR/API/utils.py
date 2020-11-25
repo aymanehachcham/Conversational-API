d8ace68bfef196e16792fd11c92bfb6e4d041f43
aymanehachcham <aymanehachchaming@gmail.com>
2020-11-25 11:18:27 +0100

    Peer API first version without test settings

---
+++ ASR/API/utils.py
@@ -0,0 +1,11 @@
+
+import uuid
+import os
+
+def get_path_media(instance, filename):
+    _, ext = os.path.splitext(filename)
+    return 'audio/{}{}'.format(uuid.uuid4(), ext)
+
+def get_path_media_audio_transformation(instance, filename):
+    _, ext = os.path.splitext(filename)
+    return 'audio_transformation/{}{}'.format(uuid.uuid4(), ext)
